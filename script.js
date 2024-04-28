4cf916152a9633a4b4c69ccae591688a2a653de1
JordanAdams_MacBook <jsadams27@gmail.com>
2024-04-28 12:30:24 -0400

    sets up objects for gameboard, player, game logic

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('hello world')
+const Gameboard = (function() {
+    let boardArr = [];
+
+    const upperLeftBox = boardArr[0];
+    const upperCenterBox = boardArr[1];
+    const upperRightBox = boardArr[2];
+    const middleLeftBox = boardArr[3];
+    const middleCenterBox = boardArr[4];
+    const middleRightBox = boardArr[5];
+    const lowerLeftBox = boardArr[6];
+    const lowerCenterBox = boardArr[7];
+    const lowerRightBox = boardArr[8];
+
+
+    return {    upperLeftBox, upperCenterBox, upperRightBox, 
+                middleLeftBox, middleCenterBox, middleRightBox,
+                lowerLeftBox, lowerCenterBox, lowerRightBox };
+
+})();
+
+// function createUser (name) {
+
+// } 
+
+// const gameLogic = (function () {
+
+// })();
