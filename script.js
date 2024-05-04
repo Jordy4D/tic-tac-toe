d3dc724d73cec2b8635dd913e2a50f1ab6e2a6b0
JordanAdams_MacBook <jsadams27@gmail.com>
2024-05-04 11:15:29 -0400

    winner check function, needs tie check

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const gameboard = (function() {
     }
 
 
-    return { board, getBoard, addMark };
+    return { getBoard, addMark };
 
 })();
 
@@ -83,9 +83,62 @@ const gameController = (function (
         )
         gameboard.addMark(row, col, getActivePlayer().mark);
     
+        getWinner();
         switchPlayerTurn();
         printNewRound();    
     };
+
+    
+    
+    
+    const getWinner = () => {
+        const topLeft = gameController.controllerBoard[0][0];
+        const topMid = gameController.controllerBoard[0][1];
+        const topRight = gameController.controllerBoard[0][2];
+        const midLeft = gameController.controllerBoard[1][0];
+        const midMid = gameController.controllerBoard[1][1];
+        const midRight = gameController.controllerBoard[1][2];
+        const botLeft = gameController.controllerBoard[2][0];
+        const botMid = gameController.controllerBoard[2][1];
+        const botRight = gameController.controllerBoard[2][2];
+
+        const horizontalWin = ( topLeft && topMid && topRight ||
+                                midLeft && midMid && midRight ||
+                                botLeft && botMid && botRight )
+
+        const veritcalWin = (   topLeft && midLeft && botLeft ||
+                                topMid && midMid && botMid ||
+                                topRight && midRight && botRight )
+
+        const diagonalWin = (   topLeft && midMid && botRight || 
+                                botLeft && midMid && topRight )
+
+        
+        // for (let i = 0 ; i <= gameController.controllerBoard.length ; i++) {
+        //     for (let j = 0 ; j <= gameController.controllerBoard.length ; j++) {
+        //         if (gameController.controllerBoard[i][j] === '') {
+        //             continue;
+        //         } else {
+        //             console.log(`it's a tie!`)
+        //         }
+        //     }
+        // }
+
+        if (horizontalWin === ( players[0].mark || players[1].mark ) ) {
+                        
+            console.log(`${getActivePlayer().name} is the winner!`)
+        
+        } else if ( veritcalWin === ( players[0].mark || players[1].mark )) {
+                        
+            console.log(`${getActivePlayer().name} is the winner!`)
+    
+        } else if ( diagonalWin === ( players[0].mark || players[1].mark ) ) {
+                        
+            console.log(`${getActivePlayer().name} is the winner!`)
+        } 
+        
+        
+    }
 
     printNewRound();
     
@@ -94,7 +147,8 @@ const gameController = (function (
         controllerBoard,
         playRound,
         switchPlayerTurn, 
-        getActivePlayer 
+        getActivePlayer,
+        getWinner
         };
 
 
